78e4b39216c1dcf87eaed7a9d857a2325d294055
sachahjkl <sacha@sacha.house>
2026-09-25 22:18:53 +0200

    Fix deployment metadata Nix lint

--- flake.nix
+++ flake.nix
@@ -37,15 +37,18 @@
         inherit (pkgs) lib;
         package = builtins.fromJSON (builtins.readFile ./package.json);
         publishedVersion = (builtins.fromJSON (builtins.readFile ./package-lock.json)).packages."node_modules/@sachahjkl/backoffice".version;
-        commit =
-          if self ? rev then self.rev
-          else if self ? dirtyRev then self.dirtyRev
-          else "0000000000000000000000000000000000000000";
+        commit = self.rev or (self.dirtyRev or "0000000000000000000000000000000000000000");
         deploymentMetadata = builtins.toJSON {
           inherit commit;
           packages = [
-            { name = "@froment/api"; version = publishedVersion; }
-            { name = "@sachahjkl/backoffice"; version = publishedVersion; }
+            {
+              name = "@froment/api";
+              version = publishedVersion;
+            }
+            {
+              name = "@sachahjkl/backoffice";
+              version = publishedVersion;
+            }
           ];
         };
         # Do not permit a release without a registry-generated lock and its verified Nix hash.
